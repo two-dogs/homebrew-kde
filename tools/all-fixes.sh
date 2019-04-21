04c83d59cdfbc8f3398979137301682f0d50118a
sl1pkn07 <sl1pkn07@gmail.com>
2019-04-22 01:24:05 +0200

    Add Gwenview and libkdcraw
    add gwenview and libkdcraw into the repo

--- tools/all-fixes.sh
+++ tools/all-fixes.sh
@@ -109,4 +109,5 @@ ln -sfv "$(brew --prefix)/opt/kcalc/bin/kcalc.app" "$HOME/Applications/KDE/"
 ln -sfv "$(brew --prefix)/share/gwenview" "$HOME/Library/Application Support"
 ln -sfv "$(brew --prefix)/opt/gwenview/bin/gwenview.app" "$HOME/Applications/KDE/"
 ln -sfv "$(brew --prefix)/opt/gwenview_importer/bin/gwenview_importer.app" "$HOME/Applications/KDE/"
+
 exit 0
